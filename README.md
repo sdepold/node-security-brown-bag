2148648dfe073113e3f8cae90b901832d3314ce1
Sascha Depold <sdepold@ebay.com>
2017-02-07 09:50:52 +0100

    Add read and create endpoint for tasks

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ yarn add --dev sequelize-cli
 node_modules/.bin/sequelize init
 node_modules/.bin/sequelize model:create --name Task --attributes title:string
 ```
+
+## API
+
+```
+# Reading all tasks
+curl localhost:3000/tasks
+
+# Creating a task
+curl localhost:3000/tasks -d '{"title":"Some title"}'  -H 'content-type:application/json'
+```
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.Task.findAll().then(function (tasks) {
+    res.send({
+      entries: tasks
+    });
+  })
+});
+
+router.post('/', function(req, res, next) {
+  models.Task.create(req.body, { fields: ['title'] }).then(function (task) {
+    res.send(task);
+  });
 });
 
 module.exports = router;
